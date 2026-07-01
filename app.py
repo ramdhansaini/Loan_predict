becb73c1b11b6e07fb5a85816d67ad171866b789
Ramdhan Saini <sainiramdhan286@gmail.com>
2026-07-01 19:38:04 +0530

    Update

--- app.py
+++ app.py
@@ -18,15 +18,22 @@ def home():
 def predict():
     age = int(request.form["age"])
     income = int(request.form["income"])
+    person_ownership = request.form["person_home_ownership"]
+    person_emp_length = int(request.form["person_emp_length"])
     loan_amount = int(request.form["loan_amount"])
+    loan_int_rate = float(request.form["loan_int_rate"])
+    loan_intent = request.form["loan_intent"]
+    loan_grade = request.form["loan_grade"]
+    loan_percent_income = float(request.form["loan_percent_income"])
+    cb_person_default_on_file = request.form["cb_person_default_on_file"]
+    cb_person_cred_hist_length = int(request.form["cb_person_cred_hist_length"])
+    
     # Add more form fields as needed
     # Example:
-    # Person_emp_length = request.form["Person_emp_length"]
-    # Person_home_ownership = request.form["Person_home_ownership"]
-    # Credit_score = int(request.form["Credit_score"])
+    #  could not convert string to float: np.str_('')
 
     # Create a numpy array with the input values
-    input_data = np.array([[age, income, loan_amount]])
+    input_data = np.array([[age, income, loan_amount, loan_int_rate, loan_intent, loan_grade, loan_percent_income, cb_person_default_on_file, cb_person_cred_hist_length  ]])
 
     # Scale the input data
     input_data_scaled = scaler.transform(input_data)
